f474aab763d252a210c06502331d8938d25e9be1
Oscar Laird <olaird25@gmail.com>
2024-02-23 07:28:43 -0500

    Add support for pgvector_remote algorithm and update Dockerfile

--- ann_benchmarks/algorithms/pgvector_remote/module.py
+++ ann_benchmarks/algorithms/pgvector_remote/module.py
@@ -2,6 +2,8 @@ import subprocess
 import sys
 import pgvector.psycopg
 import psycopg
+# from pyscopg.errors import _info_to_dict
+from pprint import pprint
 
 from ..base.module import BaseANN
 
@@ -20,25 +22,50 @@ class PGVector_Remote(BaseANN):
         else:
             raise RuntimeError(f"unknown metric {metric}")
 
+    def notice_handler(self, notice):
+        print("Received notice:", notice.message_primary)
+        # user pprint to print the notice as a dictionary
+        pprint(notice.__reduce__())
+
     def fit(self, X):
         subprocess.run("service postgresql start", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-        conn = psycopg.connect(user="ann", password="ann", dbname="ann", autocommit=True)
+        conn = psycopg.connect(user="postgres", password="postgres", dbname="ann", autocommit=True)
         pgvector.psycopg.register_vector(conn)
+        # send client messages to stdout
+        conn.add_notice_handler(self.notice_handler)
+
+
         cur = conn.cursor()
-        cur.execute("DROP TABLE IF EXISTS items")
-        cur.execute("CREATE TABLE items (id int, embedding vector(%d))" % X.shape[1])
+        # cur.execute("SET client_min_messages = debug1")
+
+        # cur.execute("CREATE TABLE items (id int, embedding vector(%d))" % X.shape[1])
+        cur.execute("CREATE TABLE items (embedding vector(%d), id int)" % X.shape[1])  # TODO: id is in the first column
         cur.execute("ALTER TABLE items ALTER COLUMN embedding SET STORAGE PLAIN")
         print("copying data...")
         with cur.copy("COPY items (id, embedding) FROM STDIN") as copy:
             for i, embedding in enumerate(X):
                 copy.write_row((i, embedding))
+        # print a couple random rows
+        cur.execute("SELECT * FROM items ORDER BY random() LIMIT 5")
+        print("sample rows:")
+        for row in cur.fetchall():
+            print(row)
         print("creating index...")
         if self._metric == "angular":
-            cur.execute(
-                "CREATE INDEX ON items USING pinecone (embedding vector_cosine_ops) WITH (spec = '{\"serverless\":{\"cloud\":\"aws\",\"region\":\"us-west-2\"}})" % (self._m, self._ef_construction)
-            )
+            # cur.execute(
+                # "CREATE INDEX ON items USING pinecone (embedding vector_cosine_ops) WITH (spec = '{\"serverless\":{\"cloud\":\"aws\",\"region\":\"us-west-2\"}})" % (self._m, self._ef_construction)
+            # )
+            pass
         elif self._metric == "euclidean":
-            cur.execute("CREATE INDEX ON items USING pinecone (embedding vector_l2_ops) WITH (spec = '{\"serverless\":{\"cloud\":\"aws\",\"region\":\"us-west-2\"}})" % (self._m, self._ef_construction))
+            print('hello Euclid')
+            cur.execute("SET ivfflat.probes = 17")
+            cur.execute("SET pinecone.api_key = '5b2c1031-ba58-4acc-a634-9f943d68822c'")
+            cur.execute("SHOW ivfflat.probes")
+            print(cur.fetchone())
+            cur.execute("SHOW pinecone.api_key")
+            print(cur.fetchone())
+            # set client debug level to debug1
+            cur.execute("CREATE INDEX pc_index ON items USING pinecone (embedding) WITH (spec = '{\"serverless\":{\"cloud\":\"aws\",\"region\":\"us-west-2\"}}')")
         else:
             raise RuntimeError(f"unknown metric {self._metric}")
         print("done!")
@@ -46,7 +73,7 @@ class PGVector_Remote(BaseANN):
 
     def set_query_arguments(self, ef_search):
         self._ef_search = ef_search
-        self._cur.execute("SET hnsw.ef_search = %d" % ef_search)
+        # self._cur.execute("SET hnsw.ef_search = %d" % ef_search)
 
     def query(self, v, n):
         self._cur.execute(self._query, (v, n), binary=True, prepare=True)
@@ -55,8 +82,9 @@ class PGVector_Remote(BaseANN):
     def get_memory_usage(self):
         if self._cur is None:
             return 0
-        self._cur.execute("SELECT pg_relation_size('items_embedding_idx')")
-        return self._cur.fetchone()[0] / 1024
+        # self._cur.execute("SELECT pg_relation_size('items_embedding_idx')")
+        # return self._cur.fetchone()[0] / 1024
+        return 1
 
     def __str__(self):
         return f"PGVector_Remote(m={self._m}, ef_construction={self._ef_construction}, ef_search={self._ef_search})"
